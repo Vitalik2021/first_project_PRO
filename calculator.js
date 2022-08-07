399ccec5da664327f07580e9ca5c76240b0289e0
Vitalii <vitalick066@gmail.com>
2022-08-07 14:17:35 +0300

    fixed func

--- calculator.js
+++ calculator.js
@@ -13,9 +13,9 @@ function closedCalculator(calc) {
         },
 
         mult: function (multiplication) {
-          `  ${calc = calc * multiplication} `
+            calc = calc * multiplication
         },
-        getResult: function (){
+        getResult: function () {
             return calc
         }
     }
@@ -23,7 +23,7 @@ function closedCalculator(calc) {
 
 const first = closedCalculator(4);
 first.add(33)
- first.sub(12);
+first.sub(12);
 first.div(2);
 first.mult(4);
 console.log(first.getResult())
